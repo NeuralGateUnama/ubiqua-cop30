26994105cf7fe6beaf107d190ae4fe1f5f437ff2
Andr3Dav1d <adavid.mendes07@proton.me>
2025-05-06 10:51:16 -0300

    Adicionando segundos no contador

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const agora = DateTime.now().setZone('America/Belem');
         const inicioCOP30 = DateTime.fromISO('2025-11-10T08:00:00', { zone: 'America/Belem' });
 
-        const diff = inicioCOP30.diff(agora, ['months', 'days', 'hours', 'minutes']).toObject();
+        const diff = inicioCOP30.diff(agora, ['months', 'days', 'hours', 'minutes', 'seconds']).toObject();
 
         if (agora >= inicioCOP30) {
             document.getElementById('contagem_reg').innerHTML = `<div style="color: #05AF7F;" >A COP30 já começou!</div>`;
@@ -16,8 +16,9 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('cont_dias').innerText = Math.floor(diff.days);
         document.getElementById('cont_horas').innerText = Math.floor(diff.hours);
         document.getElementById('cont_minutos').innerText = Math.floor(diff.minutes);
+        document.getElementById('cont_segundos').innerText = Math.floor(diff.seconds);
 
     }
-    const intervalo = setInterval(atualizarContador, 60000);
+    const intervalo = setInterval(atualizarContador, 1000);
     atualizarContador();
 });
